85835082a8297e52650bbf908bdd782ade762128
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2024-10-01 15:12:01 +0200

    added about me template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  onAppear(){
+    console.log("tu");
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {AppearDirective} from './app/appear';
+
+@NgModule({
+  exports: [
+  ]
+})
+export class PlunkerMaterialModule {}
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+
+  declarations: [AppearDirective],
+  bootstrap: [],
+  providers: []
+})
+export class PlunkerAppModule {}
+// platformBrowserDynamic().bootstrapModule(PlunkerAppModule);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
